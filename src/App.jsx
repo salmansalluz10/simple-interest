0d8a47a090d55d47f88376e6fb38908974808d5c
salmansalluz10 <salmansalluzz56@gmail.com>
2025-03-23 21:38:51 +0530

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -63,22 +63,22 @@ function App() {
   return (
     <>
      <main className='flex justify-center items-center w-full h-screen bg-red-600'>
-        <section className='w-[350px] h-[600px] flex justify-top p-[25px] items-center flex-col rounded'>
-            <h1 className='text-[26px] font-semibold'>Simple interest App</h1>
-            <p className='text-gray-800 mb-[25px]'>calculate your simple interest easily</p>
+        <section className='w-[450px] h-[600px] flex justify-top p-[30px] items-center flex-col rounded'>
+            <h1 className='text-[26px] font-semibold'>Simple Interest App</h1>
+            <p className='text-gray-800 mb-[25px]'>Calculate your simple interest easily</p>
             <div className='w-[260px] h-[140px] bg-red-400 mb-[30px] flex justify-center items-center flex-col text-white rounded'>
               <h3 className='text-[22px] font-semibold'><i className="fa-solid fa-indian-rupee-sign text-[19px]"></i>{interest}  </h3>
-              <p>Total simple interest</p>
+              <p className='text-dark'>Total simple interest</p>
             </div>
             <p className='-ml-[135px] -mb-[7px]'>Principle amount</p>
             <input className='w-[260px] h-[40px] my-[10px] rounded border border-gray-300 pl-[10px]' value={principle} type="text" name='amount' onChange={(event) => {validate(event)}} />
-            {!isPrinciple  && <span className='-ml[10px text-red-200'>*invalid input</span>}
+            {!isPrinciple  && <span className='-ml[10px text-red-600'>*invalid input</span>}
             <p className='-ml-[145px] -mb-[7px]'>Rate of Interest</p>
             <input className='w-[260px] h-[40px] my-[10px] rounded border border-gray-300 pl-[10px]' value={rate} type="text" name='interest' onChange={(event) => {validate(event)}} />
-            {!isRate  && <span className='-ml[10px text-red-200'>*invalid input</span>}
+            {!isRate  && <span className='-ml[10px text-red-600'>*invalid input</span>}
             <p className='-ml-[215px] -mb-[7px]'>Time</p>
             <input className='w-[260px] h-[40px] my-[10px] rounded border border-gray-300 pl-[10px]' type="text" value={year} name='time' onChange={(event) => {validate(event)}} />
-            {!isYear  && <span className='-ml[10px text-red-200'>*invalid input</span>}
+            {!isYear  && <span className='-ml[10px text-red-600'>*invalid input</span>}
             <div className='w-[260px] h-[100px] flex justify-between items-center'>
               <button className='btn btn-primary' disabled={isPrinciple && isRate && isYear?false:true}  onClick={handleCalculate} >Calculate</button>
               <button className='bg-gray-900 text-white font-semibold px-[16px] py-[8px] rounded' onClick={handleReset} >Reset</button>
